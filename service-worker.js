b4c561e896047d20ed4d6b6f0c95364676e4aed3
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-31 22:11:30 -0300

    resolvendo erro de rota inicial

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,39 @@
+const CACHE_NAME = 'quiz-pda-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/public/logo1.png',
+  '/public/logo1.png',
+  // Adicione mais URLs de recursos aqui
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installing...');
-  });
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activating...');
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
