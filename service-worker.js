4930b5a4c452e971998f91c0d0642785a79af2e7
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-31 21:26:26 -0300

    transformando quiz em um PWA

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+  });
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+  });
